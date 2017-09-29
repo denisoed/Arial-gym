2185bd7c366512707342738f62d28e5ded3569de
denisoed <gufig93@mail.ru>
2017-09-29 22:24:25 +0300

    Added preloader and parallax effect for section banner

--- app/js/main.js
+++ app/js/main.js
@@ -24,6 +24,29 @@ $('.btn-menu').on('click', function(){
 
 })();
 
+paraxify('.paraxify');
+
+// Preloader
+var preloader = document.getElementById("preloader");
+
+function fadeOutnojquery(el){
+  el.style.opacity = 1;
+  var interpreloader = setInterval(function(){
+    el.style.opacity = el.style.opacity - 0.05;
+
+    if (el.style.opacity <=0.05){
+      clearInterval(interpreloader);
+      preloader.style.display = "none";
+    }
+  },16);
+}
+
+window.onload = function(){
+  setTimeout(function(){
+    fadeOutnojquery(preloader);
+  },1000);
+};
+
 
 
 var filterList = {
@@ -164,7 +187,9 @@ $('.question__title').on('click', (e)=> {
 })
 
 
-$('.home-slider__box').owlCarousel({
+let owl = $('.home-slider__box');
+
+owl.owlCarousel({
   loop:true,
   autoplay: true,
   smartSpeed: 900,
@@ -173,6 +198,37 @@ $('.home-slider__box').owlCarousel({
   nav:false,
   items:1
 });
+
+// add animate.css class(es) to the elements to be animated
+function setAnimation(_elem, _InOut) {
+    // Store all animationend event name in a string.
+    // cf animate.css documentation
+    var animationEndEvent = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+
+    _elem.each(function() {
+        var $elem = $(this);
+        var $animationType = 'animated ' + $elem.data('animation-' + _InOut);
+
+        $elem.addClass($animationType).one(animationEndEvent, function() {
+            $elem.removeClass($animationType); // remove animate.css Class at the end of the animations
+        });
+    });
+}
+
+// Fired before current slide change
+owl.on('change.owl.carousel', function(event) {
+    var $currentItem = $('.owl-item', owl).eq(event.item.index);
+    var $elemsToanim = $currentItem.find("[data-animation-out]");
+    setAnimation($elemsToanim, 'out');
+});
+
+// Fired after current slide has been changed
+owl.on('changed.owl.carousel', function(event) {
+
+    var $currentItem = $('.owl-item', owl).eq(event.item.index);
+    var $elemsToanim = $currentItem.find("[data-animation-in]");
+    setAnimation($elemsToanim, 'in');
+})
 
 var filterList = {
 
